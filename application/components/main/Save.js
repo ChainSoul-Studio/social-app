d86b8f3734deee1f589005bc61d3a50e7931068b
Brandon Sievert (slick) <brandonwsievert@gmail.com>
2021-07-07 14:01:09 -0400

    Images are now saved under uid in firestore database

--- application/components/main/Save.js
+++ application/components/main/Save.js
@@ -7,7 +7,7 @@ require("firebase/firestore");
 require("firebase/firebase-storage");
 
 // Save Function
-export default function Save(props) {
+export default function Save(props, {navigation}) {
   const [caption, setCaption] = useState("");
 
   const uploadImage = async () => {
@@ -32,6 +32,7 @@ export default function Save(props) {
 
     const taskCompleted = () => {
         task.snapshot.ref.getDownloadURL().then((snapshot) => {
+            savePostData(snapshot);
             console.log(snapshot)
         })
     }
@@ -41,6 +42,20 @@ export default function Save(props) {
     }
 
     task.on('state_changed', taskProgress, taskError, taskCompleted)
+
+    const savePostData = (downloadURL) => {
+        firebase.firestore()
+                    .collection('posts')
+                    .doc(firebase.auth().currentUser.uid)
+                    .collection('userPosts')
+                    .add({
+                        downloadURL,
+                        caption,
+                        creation: firebase.firestore.FieldValue.serverTimestamp()
+                    }).then((function () {
+                        navigation.popToTop()
+                    }))
+    }
   };
 
   return (
